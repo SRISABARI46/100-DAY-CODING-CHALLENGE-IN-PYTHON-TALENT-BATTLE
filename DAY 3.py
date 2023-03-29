0ed70583736cae97107c7378dd3bd0313ba492f0
SRISABARI <87270623+SRISABARI46@users.noreply.github.com>
2023-03-29 19:53:05 +0530

    Update and rename DAY 3 to DAY 3.py
    Day 3 coding Statement: Write a program to find ASCII values of a character
    
    Description:
    
    Get an input character from the user and give the ASCII value of the given input as the output.
    
    Input:
    
    b
    
    Output:
    
    98
    
    Input:
    
    B
    
    Output:
    
    66

---
+++ DAY 3.py
@@ -0,0 +1,7 @@
+#Day 3 coding Statement: Write a program to find ASCII values of a character
+
+user_input=input("Enter a character:")
+
+ASCII=ord(user_input)
+
+print("The ASCII value for the character {} is {}".format(user_input,ASCII))
