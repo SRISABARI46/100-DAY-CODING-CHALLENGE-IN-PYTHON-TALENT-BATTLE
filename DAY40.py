16f50946ba463db5e52fa7ec5369eb5757c52819
SRISABARI <87270623+SRISABARI46@users.noreply.github.com>
2023-04-26 21:10:55 +0530

    Create DAY40.py
    Day 40 coding Statement : Write a Program to Replace substring in a string
    
    Description
    
    Get a string as input from the user and then get another string which has to be removed from the string.
    
    Get the third input, the new substring which is placed in that substring position.
    
    Finally print the output by replacing the substring with new string.
    
    Input
    
    Enter a string
    
    talentbattle
    
    Enter the substring to be removed :
    
    talent
    
    Enter the new substring :
    
    student
    
    Output
    
    The new string :
    
    studentbattle

---
+++ DAY40.py
@@ -0,0 +1,9 @@
+# Day 40 coding Statement : Write a Program to Replace substring in a string
+
+str1=input("Enter a string:")
+str2=input("Enter the substring to be removed :")
+str3=input("Enter the new substring :")
+
+str4=str1.replace(str2,str3)
+print("The new string:")
+print(str4)
