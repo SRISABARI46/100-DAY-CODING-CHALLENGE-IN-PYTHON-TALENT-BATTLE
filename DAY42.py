ecf72d6d8e7310a55274cfdf1d5a881acd530101
SRISABARI <87270623+SRISABARI46@users.noreply.github.com>
2023-04-26 21:14:58 +0530

    Create DAY42.py
    Day 42 coding Statement : Write Program to check if two arrays are the same or not
    
    Description
    
    Get two arrays as the input from the user and check whether it is the same or not.
    
    Input
    
    Enter the size of first array :
    
    3
    
    Enter the size of second array :
    
    3
    
    Enter elements of first array :
    
    1 2 3
    
    Enter elements of second array :
    
    1 2 3
    
    Output
    
    Same

---
+++ DAY42.py
@@ -0,0 +1,17 @@
+# Day 42 coding Statement : Write Program to check if two arrays are the same or not
+
+arr1=int(input("Enter the size of first array :"))
+arr2=int(input("Enter the size of second array :"))
+
+print("Enter elements of first array :")
+arr_1=input("").split()
+print(arr_1)
+    
+print("Enter elements of second array :")
+arr_2=input("").split()
+print(arr_2)
+
+if arr_1==arr_2:
+    print("Same")
+else:
+    print("Not Same")
